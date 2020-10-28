3373eac0893ac9ccac6bf48dada328b61b1a45da
delchew <delchew@mail.ru>
2020-10-28 03:51:26 +0300

    Add classes to working with files and json

---
+++ CablesCraftMobile/DataWorkers/JsonRepository.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace CablesCraftMobile
+{
+    public class JsonRepository
+    {
+        public IList<T> GetObjects<T>(FileInfo dataFileInfo, string jsonDataPath)
+        {
+            var selectedJsonData = GetCurrentJsonDataToDeserialize(dataFileInfo, jsonDataPath);
+            return JsonConvert.DeserializeObject<List<T>>(selectedJsonData);
+        }
+
+        public IList<T> GetObjects<T>(FileInfo dataFileInfo)
+        {
+            return GetObjects<T>(dataFileInfo, string.Empty);
+        }
+
+        public IDictionary<TKey, TValue> GetObjects<TKey, TValue>(FileInfo dataFileInfo, string jsonDataPath)
+        {
+            var selectedJsonData = GetCurrentJsonDataToDeserialize(dataFileInfo, jsonDataPath);
+            return JsonConvert.DeserializeObject<Dictionary<TKey, TValue>>(selectedJsonData);
+        }
+
+        public IDictionary<TKey, TValue> GetObjects<TKey, TValue>(FileInfo dataFileInfo)
+        {
+            return GetObjects<TKey, TValue>(dataFileInfo, string.Empty);
+        }
+
+        private string GetCurrentJsonDataToDeserialize(FileInfo dataFileInfo, string jsonDataPath)
+        {
+            if (dataFileInfo.Exists)
+            {
+                var loadedData = File.ReadAllText(dataFileInfo.FullName, Encoding.UTF8);
+                if (!string.IsNullOrEmpty(loadedData))
+                {
+                    if (!string.IsNullOrEmpty(jsonDataPath))
+                    {
+                        var jObject = JObject.Parse(loadedData);
+                        var jsonSelectedData = jObject.SelectToken(jsonDataPath);
+                        return jsonSelectedData.ToString();
+                    }
+                    return loadedData;
+                }
+                throw new FileLoadException($"Отсутствуют данные в файле {dataFileInfo.Name}!");
+            }
+            throw new FileNotFoundException($"Файл {dataFileInfo.Name} не найден! Проверьте правильность пути к файлу.");
+        }
+    }
+}
